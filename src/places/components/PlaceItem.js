3e59c7d565538c572ac69ed3e21f710cd82a33f6
Azadeh Hakkak <azadeh.hakkak@gmail.com>
2021-02-08 18:24:11 +1100

    sharing input values from Input to NewPlace component

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useCallback } from "react";
 
-import { VALIDATOR_REQUIRE } from "../../shared/Util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/Util/validators";
 import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {
+    // we want to manage overall validity and values of the entire form.
+  }, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         type="text"
         label="Title"
         element="input"
         validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid Title."
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        label="Description"
+        element="textarea"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description(at least 5 characters)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
