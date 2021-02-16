be5b1b5d1be11c7af66a8fef580d468d88d358b6
Azadeh Hakkak <azadeh.hakkak@gmail.com>
2021-02-16 13:21:58 +1100

    adding auth context, adding authenticated and unauthenticated routes

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import { useForm } from "../../shared/hooks/form-hook";
@@ -9,10 +9,12 @@ import {
   VALIDATOR_EMAIL,
   VALIDATOR_REQUIRE,
 } from "../../shared/Util/validators";
-
+import { AuthContext } from "../../shared/context/auth.context";
 import "./Auth.css";
 
 const Auth = () => {
+  const auth = useContext(AuthContext);
+
   const [isLoginMode, setIsLoginMode] = useState(true);
 
   const [formState, inputHandler, setFormData] = useForm(
@@ -61,7 +63,8 @@ const Auth = () => {
 
   const authSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(formState);
+    console.log(formState.inputs);
+    auth.login();
   };
 
   return (
